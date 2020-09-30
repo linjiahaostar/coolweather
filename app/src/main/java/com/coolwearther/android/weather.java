7aa33df335422351253645273003e0a80c6230b3
linjiahaostar <linjiahaostar@163.com>
2020-09-30 10:35:51 +0800

    commit

---
+++ app/src/main/java/com/coolwearther/android/weather.java
@@ -0,0 +1,12 @@
+package com.coolwearther.android;
+
+/**
+ * @ProjectName: CoolGITWearther
+ * @Package: com.coolwearther.android
+ * @ClassName: weather
+ * @Description: java类作用描述
+ * @Author: linjiahao
+ * @CreateDate: 2020/9/30 10:31 AM
+ */
+public class weather {
+}
